468cc3384e8637de7a48fcd8b447911b53ea14dd
jrkozak92 <jrkozak92@gmail.com>
2022-02-12 15:43:34 -0600

    Tracker: Added Nav, styling; Share: Built routes up to Update, added image hosting and processing

---
+++ models/shareSchema.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const shareSchema = new mongoose.Schema({
+  title: {type: String, required: true},
+  content: String,
+  img: {
+    data: Buffer,
+    contentType: String
+  },
+  comments: [
+    {text: String,
+    date: Date}
+  ]
+})
+
+const Share = mongoose.model('Share', shareSchema)
+
+module.exports = Share
--- server.js
+++ server.js
@@ -8,6 +8,24 @@ const app = express ();
 const db = mongoose.connection;
 require('dotenv').config()
 const Task = require('./models/taskSchema.js')
+const Share = require('./models/shareSchema.js')
+//Node Modules
+const fs = require('fs')
+const path = require('path')
+
+//Image Handler Package: Multer
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './public/uploads')
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+})
+
+const upload = multer({storage: storage})
 
 //___________________
 //Port
@@ -51,7 +69,7 @@ app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 //___________________
 //localhost:3000
 app.get('/' , (req, res) => {
-  res.send('Hello World!');
+  res.redirect('/tracker');
 });
 //___________________
 //  Tracker
@@ -60,7 +78,7 @@ app.get('/' , (req, res) => {
 //Index: /tracker/
 app.get('/tracker', (req, res) => {
   Task.find({}, (err, tasks) => {
-    res.render('index.ejs',
+    res.render('./tracker/index.ejs',
       {
         title: 'Tracker | Home',
         tasks: tasks
@@ -71,7 +89,7 @@ app.get('/tracker', (req, res) => {
 
 //New: /tracker/new
 app.get('/tracker/new', (req, res) => {
-  res.render('new.ejs',
+  res.render('./tracker/new.ejs',
     {
       title: 'Tracker | Add'
     })
@@ -88,7 +106,7 @@ app.post('/tracker', (req, res) => {
 //Show: /tracker/:id
 app.get('/tracker/:id', (req, res) => {
   Task.findById(req.params.id, (err, task) => {
-    res.render('show.ejs',
+    res.render('./tracker/show.ejs',
       {
         title: 'Tracker | Item',
         task: task
@@ -100,7 +118,7 @@ app.get('/tracker/:id', (req, res) => {
 //Edit: /tracker/:id/edit
 app.get('/tracker/:id/edit', (req, res) => {
   Task.findById(req.params.id, (err, task) => {
-    res.render('edit.ejs',
+    res.render('./tracker/edit.ejs',
       {
         title: 'Tracker | Edit',
         task: task
@@ -130,6 +148,88 @@ app.delete('/tracker/:id', (req, res) => {
     res.redirect('/tracker')
   })
 })
+
+//___________________
+//  Share
+//___________________
+
+//Index: /share/
+app.get('/share', (req, res) => {
+  Share.find({}, (err, shares) => {
+    res.render('./share/index.ejs',
+      {
+        title: 'Share | Home',
+        shares: shares
+      })
+  })
+})
+
+//New: /share/new
+app.get('/share/new', (req, res) => {
+  res.render('./share/new.ejs',
+    {
+      title: 'Share | Add'
+    })
+})
+// Got Image handling here: https://www.geeksforgeeks.org/upload-and-retrieve-image-on-mongodb-using-mongoose/
+//Create: /share
+app.post('/share', upload.single('img'), (req, res, next) => {
+  let shareObj = {
+    title: req.body.title,
+    content: req.body.content,
+    img: {
+      data: fs.readFileSync(path.join('./public/uploads/' + req.file.filename)),
+      contentType: 'image/png'
+    }
+  }
+  console.log('Share Object: ', shareObj);
+  Share.create(shareObj, (err, share) => {
+    console.log(share)
+    res.redirect('/share')
+  })
+})
+
+//Show: /share/:id
+app.get('/share/:id', (req, res) => {
+  Share.findById(req.params.id, (err, share) => {
+    res.render('./share/show.ejs',
+      {
+        title: 'Share | Item',
+        share: share
+      })
+    })
+  }
+)
+
+//Edit: /share/:id/edit
+app.get('/share/:id/edit', (req, res) => {
+  Share.findById(req.params.id, (err, share) => {
+    res.render('./share/edit.ejs',
+      {
+        title: 'Share | Edit',
+        share: share
+      })
+    })
+  }
+)
+
+//Update: /share/:id
+app.put('/share/:id', (req, res) => {
+  Share.findByIdAndUpdate(req.params.id, req.body, {new:true}, (err, update) => {
+    console.log(update)
+    res.redirect('/share')
+  })
+})
+
+//Delete: /share/:id
+app.delete('/share/:id', (req, res) => {
+  // Share.findByIdAndRemove(req.params.id, (err, removedShare) => {
+  //   console.log(removedShare)
+  //   res.redirect('/share')
+  // })
+})
+
+
 
 //___________________
 //Listener
